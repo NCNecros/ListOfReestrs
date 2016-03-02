97a7685ee84ca45b3cf1c061d1be8c4dcd8e9658
Stanislav Naydeny <n.c.necros@gmail.com>
2016-03-02 08:18:06 +0300

    add empty test class

--- src/main/java/Controller.kt
+++ src/main/java/Controller.kt
@@ -164,43 +164,47 @@ class Controller {
         }
     }
 
-    private fun parseExcelFile(xlsFile: Path): Schfakt {
+     fun parseExcelFile(xlsFile: Path): Schfakt {
         val schet = Schfakt()
-        val inputStream = FileInputStream(xlsFile.toFile())
-        val wb = WorkbookFactory.create(inputStream)
-        val sheetOne = wb.getSheetAt(0)
-        val rowThree = sheetOne.getRow(2)
-        val cell = rowThree.getCell(0)
-        val monts = "(Декабрь|Январь|Март|Апрель|Май|Июнь|Июль|Август|Сентябрь|Октябрь|Ноябрь)"
-        val types = "(основной|дополнительный|повторный)"
-        val regexGroups = " к реестру счетов №(\\d{1,5}) от (\\d{2}\\.\\d{2}.\\d{4}) за \\d{4} ($monts) ($types) по .+".toRegex().find(cell.stringCellValue)
+         try {
+             val inputStream = FileInputStream(xlsFile.toFile())
+             val wb = WorkbookFactory.create(inputStream)
+             val sheetOne = wb.getSheetAt(0)
+             val rowThree = sheetOne.getRow(2)
+             val cell = rowThree.getCell(0)
+             val monts = "(Декабрь|Январь|Март|Апрель|Май|Июнь|Июль|Август|Сентябрь|Октябрь|Ноябрь)"
+             val types = "(основной|дополнительный|повторный)"
+             val regexGroups = " к реестру счетов №(\\d{1,5}) от (\\d{2}\\.\\d{2}.\\d{4}) за \\d{4} ($monts) ($types) по .+".toRegex().find(cell.stringCellValue)
 
-        schet.dateOfReestr = regexGroups?.groups?.get(2)?.value
-        schet.month = regexGroups?.groups?.get(3)?.value
-        schet.typeOfReestr = regexGroups?.groups?.get(5)?.value
-        val sheetTwo = wb.getSheetAt(1)
+             schet.dateOfReestr = regexGroups?.groups?.get(2)?.value
+             schet.month = regexGroups?.groups?.get(3)?.value
+             schet.typeOfReestr = regexGroups?.groups?.get(5)?.value
+             val sheetTwo = wb.getSheetAt(1)
 
-        for (row in arrayOf(15, 17, 22, 24, 19, 26, 30, 31, 35, 36, 37, 38, 39, 40, 41)) {
-            val cellWithPrice = sheetTwo.getRow(row).getCell(9).stringCellValue
-            if (!cellWithPrice.equals("-")) {
-                when (row) {
-                    15, 41 -> {
-                        schet.typeOfHelp = "Стационар"
-                        schet.description = sheetTwo.getRow(row).getCell(4).stringCellValue
-                    }
-                    22, 24 -> {
-                        schet.typeOfHelp = "Дневной стационар"
-                        schet.description = sheetTwo.getRow(row).getCell(4).stringCellValue
+             for (row in arrayOf(15, 17, 22, 24, 19, 26, 30, 31, 35, 36, 37, 38, 39, 40, 41)) {
+                 val cellWithPrice = sheetTwo.getRow(row).getCell(9).stringCellValue
+                 if (!cellWithPrice.equals("-")) {
+                     when (row) {
+                         15, 41 -> {
+                             schet.typeOfHelp = "Стационар"
+                             schet.description = sheetTwo.getRow(row).getCell(4).stringCellValue
+                         }
+                         22, 24 -> {
+                             schet.typeOfHelp = "Дневной стационар"
+                             schet.description = sheetTwo.getRow(row).getCell(4).stringCellValue
 
-                    }
-                    else -> {
-                        schet.typeOfHelp = "Поликлиника"
-                        schet.description = sheetTwo.getRow(row).getCell(4).stringCellValue
-                    }
-                }
-            }
-        }
-        schet.price = sheetOne.getRow(20).getCell(13).stringCellValue.replace(" ", "").toDouble()
+                         }
+                         else -> {
+                             schet.typeOfHelp = "Поликлиника"
+                             schet.description = sheetTwo.getRow(row).getCell(4).stringCellValue
+                         }
+                     }
+                 }
+             }
+             schet.price = sheetOne.getRow(20).getCell(13).stringCellValue.replace(" ", "").toDouble()
+         }catch (e: FileNotFoundException){
+             println("Не удается найти файл")
+         }
         return schet
     }
 
