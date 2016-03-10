7d51e4312597427d359903fe89d6669c48556690
Stanislav Naydeny <n.c.necros@gmail.com>
2016-03-10 13:10:01 +0300

    fix sorting, autofilter

--- src/test/java/com/example/ControllerTest.kt
+++ src/test/java/com/example/ControllerTest.kt
@@ -1,9 +1,24 @@
 package com.example
 
+import org.junit.Test
+import org.mockito.Mockito.*;
+import org.junit.Assert.*;
+import java.io.File
+
 /**
  * Created by Necros on 01.03.2016.
  */
 class ControllerTest {
     val controller = Controller()
+
+    @Test
+    fun testListOfFilesWithIncorrectIncomingFiles(){
+        val list = mutableListOf(File("d:\\1.zip"))
+
+        val result = controller.getListOfFiles(files = list)
+        assertEquals(result.size,0)
+    }
+
+
 
 }
